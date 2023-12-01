67ecbec195cb1c0dae9fabd2a011a69c3cc92292
binhntph28014 <binhntph28014@fpt.edu.vn>
2023-12-01 08:18:34 +0700

    first commit

---
+++ app/src/main/java/binhntph28014/fpoly/bai_lam_them12/Message.java
@@ -0,0 +1,31 @@
+package binhntph28014.fpoly.bai_lam_them12;
+
+public class Message {
+    private String userId;
+    private String messageText;
+
+    public Message() {
+
+    }
+
+    public Message(String userId, String messageText) {
+        this.userId = userId;
+        this.messageText = messageText;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getMessageText() {
+        return messageText;
+    }
+
+    public void setMessageText(String messageText) {
+        this.messageText = messageText;
+    }
+}
